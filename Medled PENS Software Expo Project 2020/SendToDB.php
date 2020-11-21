10197ad69506acb15c8ae9d91f0da3edded7c4c5
zack-cpp <neojoyko@gmail.com>
2020-11-21 17:25:39 +0700

    Separate repository for Medled

--- Medled PENS Software Expo Project 2020/SendToDB.php
+++
@@ -1,9 +0,0 @@
-<?php include_once('connection.php');
-?>
-<?php
-	$ssql = "SELECT * FROM rak Limit 10";
-	$query = mysqli_query($conn,$ssql);
-	while($row = mysqli_fetch_array($query)){
-		echo $row['statuss'].",";
-	}
-?>
